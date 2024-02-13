a82b6eb2b106f722c63972ecbd7e559cea9dca19
whiitees <142717675+whiitees@users.noreply.github.com>
2024-02-13 17:54:08 +0100

    Add files via upload

---
+++ main20.java
@@ -0,0 +1,19 @@
+import java.security.SecureRandom;
+
+public class PasswordGenerator {
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+";
+
+    public static String generatePassword(int length) {
+        SecureRandom random = new SecureRandom();
+        StringBuilder password = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            password.append(CHARACTERS.charAt(random.nextInt(CHARACTERS.length())));
+        }
+        return password.toString();
+    }
+
+    public static void main(String[] args) {
+        String password = generatePassword(12);
+        System.out.println("Generated password: " + password);
+    }
+}
